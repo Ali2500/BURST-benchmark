ebbc428ad0476375e071c12f3e301148c728304d
Ali Athar <aliathar94@gmail.com>
2023-07-18 15:16:15 +0200

    removed more unused stuff

--- burstapi/thirdparty/trackeval/__init__.py
+++ burstapi/thirdparty/trackeval/__init__.py
@@ -1,4 +1,3 @@
 from burstapi.thirdparty.trackeval import datasets
 from burstapi.thirdparty.trackeval import metrics
-from burstapi.thirdparty.trackeval import plotting
 from burstapi.thirdparty.trackeval import utils
--- setup.py
+++ setup.py
@@ -22,8 +22,7 @@ VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'tqdm', 'opencv-python', 'numpy'
-    # 'requests', 'maya', 'records',
+    'tqdm', 'opencv-python', 'numpy', 'Pillow', 'tabulate', 'scipy', 'pycocotools', 
 ]
 
 # What packages are optional?
